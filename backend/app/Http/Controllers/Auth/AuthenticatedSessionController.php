3deedc6e6e9d8eef4b2c408c4f05482250aa480c
thiago8844 <thiagoaxe4@gmail.com>
2025-08-24 00:51:31 -0300

    Setup Breeze

---
+++ backend/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Database\Eloquent\Casts\Json;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class AuthenticatedSessionController extends Controller
+{
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(LoginRequest $request): JsonResponse
+    {
+        $request->authenticate();
+
+        $token = $request->user()->createToken('auth_token')->plainTextToken;
+
+        return response()->json(['token' => $token, 'user' => $request->user()], 200);
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): JsonResponse
+    {
+       $request->user()->currentAccessToken()->delete();
+
+       return response()->json(['message' => 'Logged out'], 200);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,22 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    
+    Route::post('/logout', [\App\Http\Controllers\Auth\AuthenticatedSessionController::class, 'destroy'])
+        ->name('logout');
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
+
+
+Route::post('/login', [\App\Http\Controllers\Auth\AuthenticatedSessionController::class, 'store'])
+    ->name('login');
+Route::post('/cadastro', [RegisteredUserController::class, 'store'])
+    ->name('register');
